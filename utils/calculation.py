4fee0b3981da7183dd4fe4a37d14564875529522
Eric Brown <pdcxs@outlook.com>
2020-03-15 17:43:10 +0800

    Add some useful rate functions

--- utils/calculation.py
+++ utils/calculation.py
@@ -1,5 +1,6 @@
 # from @pdcxs
-# This function calculates the intersect position of two lines
+
+# get_intersect function calculates the intersect position of two lines
 # when two lines are paralleled to each other
 # it will return `parallel` parameter
 # For example: 
@@ -28,3 +29,54 @@ def get_intersect(line1, line2, parallel):
         x = (b2 * c1 - b1 * c2) / determinant
         y = (a1 * c2 - a2 * c1) / determinant
         return x * RIGHT + y * UP
+
+# some useful rate functions
+import math
+
+def easeOutBounce(t):
+    if t < 1 / 2.75:
+        return 7.5625 * t * t
+    elif t < 2 / 2.75:
+        c = t - 1.5/2.75
+        return 7.5625 * c * c + 0.75
+    elif t < 2.5 / 2.75:
+        c = t - 2.25 / 2.75
+        return 7.5625 * c * c + .9375
+    else:
+        c = t - 2.625 / 2.75
+        return 7.5625 * c * c + .984375
+
+def easeInBounce(t):
+    return 1 - easeOutBounce(1 - t)
+
+def easeInOutBounce(t):
+    if t < 0.5:
+        return easeInBounce(2 * t)
+    else:
+        return easeOutBounce(2 * t - 1)
+    
+# Because easeOutElastic function will return
+# value greater than 1
+# There has some requirements:
+# 1. Cannot be used in MoveAlongPath animation, because
+#    points_from_proportion will crash.
+# 2. Need to remove all the np.clip functions in
+#    manimlib/animation/animation.py, i.e. change
+#    the code of get_sub_alpha and interpolate methods
+#    from `return np.clip((value - lower), 0, 1)`
+#    to `value - lower`
+#    and remove `alpha = np.clip(alpha, 0, 1)`, respectively.`
+
+def easeOutElastic(t):
+    s, a = 1.70158, 1
+    
+    if t == 0: return 0
+    if t == 1: return 1
+
+    p = 0.3
+    if a < 1:
+        a, s = 1, p / 4
+    else:
+        s = p / (2 * math.pi) * math.asin(1 / a)
+
+    return a * pow(2, -10 * t) * math.sin((t - s) * (2 * math.pi) / p) + 1
