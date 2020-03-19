1cbc4e651d7298085f9154e51c2e70cc5c3824b4
cigar666 <37494715+cigar666@users.noreply.github.com>
2020-03-19 20:27:20 +0800

    Update imports.py

--- utils/imports.py
+++ utils/imports.py
@@ -10,6 +10,7 @@ from manim_sandbox.utils.mobjects.Right_angle import *
 from manim_sandbox.utils.mobjects.angle import *
 from manim_sandbox.utils.mobjects.PeriodicTable import *
 from manim_sandbox.utils.mobjects.Shadow_around import *
+from manim_sandbox.utils.mobjects.ThreeBody import *
 
 from manim_sandbox.utils.scenes.bilibili import *
 from manim_sandbox.utils.scenes.parabola import *
