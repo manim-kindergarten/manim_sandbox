f0a886c13a3b4ded5075edafc942a3632cb7147f
Tony031218 <975062472@qq.com>
2020-04-10 09:08:21 +0800

    add left progress bar

--- utils/mobjects/VideoProgressBar.py
+++ utils/mobjects/VideoProgressBar.py
@@ -1,4 +1,4 @@
-# from @cigar666
+# from @cigar666 & @鹤翔万里
 from manimlib.imports import *
 
 class VideoProgressBar(VGroup):
@@ -45,6 +45,68 @@ class VideoProgressBar(VGroup):
             self.texts[i].move_to(self.lines[i+1])
 
         self.add(self.lines, self.texts)
+
+
+class LeftProgressBar(Scene):
+    '''
+    用于导出制作左侧进度条上文字的场景
+    目前细节需要微调
+    推荐导出5120x2880像素，带透明度的pbg图像
+    '''
+    CONFIG = {
+        'methods_dict': {
+            '序言': '0025', 
+            'shift': '0210', 
+            'move_to': '0300',
+            'scale': '0402',
+            'rotate': '0504', 
+            'flip': '0712', 
+            'stretch': '0901',
+            'to_corner': '1014', 
+            'align_to': '1129',
+            'next_to': '1227', 
+            'set_width\nset_height': '1500',
+        },
+        'total_time': '1706',
+        'text_font': "Consolas",
+        'text_scale_factor': 0.7,
+    }
+
+    def construct(self):
+        func_time = lambda t: int(t[0:2]) * 60 + int(t[2:])
+        func_loc  = lambda t: func_time(t) / func_time(total_time) * FRAME_HEIGHT * UP + FRAME_HEIGHT * DOWN / 2
+        p_list = [FRAME_HEIGHT * DOWN / 2]
+        for v in methods_dict.values():
+            p_list.append(func_loc(v))
+        p_list.append(func_loc(total_time))
+        print(p_list)
+
+        texts = VGroup(
+            *[
+                Text(text, color=WHITE, font=self.text_font, size=0.2, background_stroke_color=WHITE).scale(self.text_scale_factor)
+                for text in methods_dict.keys()
+            ]
+        )
+        texts[-1].become(
+            VGroup(
+                Text('set_width', color=WHITE, font=self.text_font, size=0.2, background_stroke_color=WHITE).scale(self.text_scale_factor),
+                Text('set_height', color=WHITE, font=self.text_font, size=0.2, background_stroke_color=WHITE).scale(self.text_scale_factor)
+            ).arrange(DOWN, buff=0.04)
+        )
+        times = VGroup(
+            *[
+                Text("{}:{}".format(time[:2], time[2:]), color=WHITE, font=self.text_font, size=0.2, background_stroke_color=WHITE).scale(0.55)
+                for time in methods_dict.values()
+            ]
+        )
+        
+        for i in range(len(methods_dict)):
+            times[i].next_to(texts[i], DOWN, buff=0.05)
+            texts[i].add(times[i])
+            texts[i].move_to(p_list[i + 1])
+
+        self.add(texts)
+
 
 ## to be tested ##
 
