2459dbecf2f655183e9cbfaf9540088cc182fc3e
Tony031218 <975062472@qq.com>
2020-06-15 09:38:12 +0800

    update DocString

--- utils/scenes/NewGraphScene.py
+++ utils/scenes/NewGraphScene.py
@@ -8,19 +8,22 @@ from @GZTime
 from manimlib.imports import *
 
 class NewGraphScene(GraphScene):
-    """
-    NewGraphScene by GZTime.
-    ~~~~~~~~~~~~~~~~
+    """NewGraphScene by GZTime.
+
     To use:
+
     - 更新函数展示区域，定义域和坐标轴位置同GraphScene，此项设置需要在setup_axes前完成。
+    
     >>> self.graph_origin = ORIGIN
     >>> self.x_min, self.x_max = -5,5
     >>> self.y_min, self.y_max = -5,5
 
     - 新建坐标轴 create axes
+   
     >>> self.setup_axes(animate=True)
 
     - 绘制函数图像 draw function graph
+    
     >>> self.add_function_graph(
             lambda x: x**x,
             animate=True,x_min=0)
@@ -32,6 +35,7 @@ class NewGraphScene(GraphScene):
             animate=True)
 
     - 绘制参数方程图像 draw parametric function graph
+    
     >>> self.add_parametric_graph(
             lambda t: [2*math.sin(t) + 2,math.cos(t) + 1,0],
             animate=True,t_min=0,t_max=2*math.pi,
@@ -42,10 +46,12 @@ class NewGraphScene(GraphScene):
             color=ORANGE)
 
     - 访问已经新建的函数使用如下两个`list`也可以直接获取add_graph函数的返回值。
+    
     >>> self.play(FadeOut(self.functions[0]))
     >>> self.play(FadeOut(self.parametric_functions[0]))
     
     - 更新函数方程 updata graph
+    
     >>> self.updata_graph(
             self.parametric_functions[0],
             lambda t: [2*math.cos(t),math.sin(t),0])
@@ -56,8 +62,7 @@ class NewGraphScene(GraphScene):
     parametric_functions = []
 
     def add_function_graph(self,func,animate=False,color=None,x_min=None,x_max=None,**kwargs):
-        """
-        向场景中添加函数图像。
+        """向场景中添加函数图像。
         Add a function graph to scene.
         """
         if color is None:
@@ -90,8 +95,7 @@ class NewGraphScene(GraphScene):
         return self
 
     def add_parametric_graph(self,func,animate=False,color=None,x_min=None,x_max=None,t_min=0,t_max=1,**kwargs):
-        """
-        向场景中添加参数方程。
+        """向场景中添加参数方程。
         Add a parametric function graph to scene.
         """
         if color is None:
@@ -119,8 +123,7 @@ class NewGraphScene(GraphScene):
         return self
 
     def updata_graph(self,graph,newfunc):
-        """
-        更新某图像的方程，并使用Transform变换。
+        """更新某图像的方程，并使用Transform变换。
         update and transform your graph to a new function.
         """
         newgraph = graph.copy().updata_function(newfunc)
@@ -128,9 +131,8 @@ class NewGraphScene(GraphScene):
         graph = newgraph
 
 class NewParametricFunction(ParametricFunction):
-    """
-    NewParametricFunction by GZTime.
-    ~~~~~~~~~~~~~~~~~~
+    """NewParametricFunction by GZTime.
+    
     此类主要覆写了generate_points函数使其生成的路径具有x轴以及y轴的限制，并将每一段曲线作为一个submobject展示。
     """
     CONFIG = {
