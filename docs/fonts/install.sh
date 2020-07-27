bc76bc2ea6993fd4ed40ea57c9accdeaa595b9f7
Tony031218 <975062472@qq.com>
2020-07-27 23:13:29 +0800

    update

--- docs/fonts/install.sh
+++ docs/fonts/install.sh
@@ -1,5 +1,5 @@
 sudo mkdir /usr/share/fonts/Consolas
-sudo cp docs/fonts/* /usr/share/fonts/Consolas/
+sudo cp fonts/* /usr/share/fonts/Consolas/
 sudo apt install ttf-mscorefonts-installer -y
 sudo apt install fontconfig -y
 sudo mkfontscale
