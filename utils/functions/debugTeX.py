0dd6e91b2b034adf414be5bcbfd6d830839b1949
widcardw <55699713+widcardw@users.noreply.github.com>
2020-11-18 16:12:59 +0800

    Update debugTeX.py

--- utils/functions/debugTeX.py
+++ utils/functions/debugTeX.py
@@ -7,3 +7,56 @@ def debugTeX(self, texm, scale_factor=0.6, text_color=PURPLE):
         tex_id = Text(str(i), font="Consolas").scale(scale_factor).set_color(text_color)
         tex_id.move_to(j)
         self.add(tex_id)
+        
+        
+# 下面的仅仅是将它封装成类，或许可以便于操作
+# 将数字的显示改成使用Integer，以避免生成过多的TextSVG
+# widcardw
+
+# 显示所有锚点的索引
+class AllPointsIndex(VGroup):
+    CONFIG = {
+        "scale_factor": 0.5,
+        "color": PURPLE,
+    }
+
+    def __init__(self, obj, **kwargs):
+        # digest_config(self, kwargs)
+        VGroup.__init__(self, **kwargs)
+        for index, points in enumerate(obj.get_all_points()):
+            point_id = Integer(index, background_stroke_width=2) \
+                .scale(self.scale_factor).set_color(self.color)
+            point_id.move_to(points)
+            self.add(point_id)
+
+            
+# 显示单个vmobject的索引
+class PointIndex(VGroup):
+    CONFIG = {
+        "scale_factor": 0.5,
+        "color": PURPLE,
+    }
+
+    def __init__(self, obj, **kwargs):
+        # digest_config(self, kwargs)
+        VGroup.__init__(self, **kwargs)
+        for index, points in enumerate(obj.get_points()):
+            point_id = Integer(index, background_stroke_width=2) \
+                .scale(self.scale_factor).set_color(self.color)
+            point_id.move_to(points)
+            self.add(point_id)
+
+
+class TexIndex(VGroup):
+    CONFIG = {
+        "scale_factor": 0.5,
+        "color": PURPLE,
+    }
+
+    def __init__(self, obj, **kwargs):
+        VGroup.__init__(self, **kwargs)
+        for index, single_tex in enumerate(obj):
+            tex_index = Integer(index, background_stroke_width=2) \
+                .scale(self.scale_factor).set_color(self.color)
+            tex_index.move_to(single_tex.get_center())
+            self.add(tex_index)
