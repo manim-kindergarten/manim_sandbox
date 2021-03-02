40ad24ecb331b5f432a75355c5315b1f1c25f797
136108Haumea <61341382+136108Haumea@users.noreply.github.com>
2021-03-03 00:12:44 +0800

    Update imports.py

--- utils/imports.py
+++ utils/imports.py
@@ -20,6 +20,7 @@ from manim_sandbox.utils.mobjects.ThreeDVector import *
 from manim_sandbox.utils.mobjects.BezierGenerator import BezierGenerator
 from manim_sandbox.utils.mobjects.BezierGenerator import BezierFunc
 from manim_sandbox.utils.mobjects.BooleanOperationsOnPolygons import PolygonIntersection, PolygonUnion, PolygonSubtraction
+from manim_sandbox.utils.mobjects.DelaunayTrianglation import DelaunayTrianglation, Voronoi
 
 from manim_sandbox.utils.scenes.bilibili import *
 from manim_sandbox.utils.scenes.parabola import *
