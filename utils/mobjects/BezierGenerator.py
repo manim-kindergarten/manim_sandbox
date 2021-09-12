66dc7240ea8763486c3b72529be429e4d8490f85
widcardw <19190218@stu.njnu.edu.cn>
2021-09-12 17:09:54 +0800

    Update BezierGenerator to manimgl version

--- utils/mobjects/BezierGenerator.py
+++ utils/mobjects/BezierGenerator.py
@@ -1,9 +1,9 @@
-from manimlib.imports import *
+from manimlib import *
 
 
 # 用无数段3阶贝塞尔曲线来拟合n阶贝塞尔曲线
 # 这只是权宜之计,今后可能会研究怎样直接绘制n阶贝塞尔曲线
-class BezierFunc(ParametricFunction):
+class BezierFunc(ParametricCurve):
     CONFIG = {
         "steps_per_handle": 80,  # 每增加一个锚点,增加的曲线数量
     }
@@ -11,24 +11,34 @@ class BezierFunc(ParametricFunction):
     def __init__(self, points, **kwargs):
         digest_config(self, kwargs)
         n = len(points) - 1
-        self.step_size = 1 / (n * self.steps_per_handle)
-        self.function = bezier(points)
-        super().__init__(**kwargs)
+        # manimgl 更新, 需要传入参数
+        super().__init__(
+            bezier(points),  # 贝塞尔曲线方程
+            [0, 1, 1 / (n * self.steps_per_handle)],  # t 范围和步长
+            **kwargs
+        )
 
 
 class BezierGenerator(VGroup):
     CONFIG = {
-        "dot_color": WHITE,
-        "dot_scale": 1,
-        "line_color": WHITE,
-        "line_stroke_width": 2,
-        "target_dot_color": RED,
-        "tar_dot_scale": 1,
-        "bezier_curve_color": ORANGE,
-        "bzc_stroke_width": 6
+        "dot_color": WHITE,            # 锚点颜色
+        "dot_scale": 1,                # 锚点大小
+        "line_color": WHITE,           # 连线颜色
+        "line_stroke_width": 2,        # 连线线宽
+        "target_dot_color": RED,       # 绘制贝塞尔曲线的那个点的颜色
+        "tar_dot_scale": 1,            # 绘制贝塞尔曲线的那个点的大小
+        "bezier_curve_color": ORANGE,  # 贝塞尔曲线的颜色
+        "bzc_stroke_width": 6          # 贝塞尔曲线的线宽
     }
 
     def __init__(self, dot_list, **kwargs):
+        """
+        传入一个点集, 根据点集创建锚点和手柄
+        产生的对象继承于 VGroup, 可以用类数组的方法进行索引
+        b[0]=点集, b[1]=直线集
+        b.curve_path=贝塞尔曲线
+        """
+        assert(len(dot_list) >= 2)
         VGroup.__init__(self, **kwargs)
         dot_color = self.dot_color
         line_color = self.line_color
@@ -82,22 +92,65 @@ class BezierGenerator(VGroup):
         # 另外,put_start_and_end_on不支持三维,如果要用到三维那么可能只能用become了
 
 
-# 使用例desu
+'''
+class MakeBezier(Animation):  # 尚不完善, 路径的绘制有问题, 暂时先用MakeBezier2
+    CONFIG = {
+        "rate_func": smooth,
+        "run_time": 6,
+    }
+
+    def __init__(self, mobject, **kwargs):
+        super().__init__(mobject, **kwargs)
+        assert (isinstance(mobject, BezierGenerator))
+        self.mobject = mobject
+        self.curve_path = mobject.curve_path
+        self.dot_anim = mobject.dot_anim
+        self.sync_line = mobject.sync_line
+
+    def interpolate_mobject(self, alpha):
+        self.dot_anim(self.mobject[0], alpha)  # 查阅UpdateFromAlphaFunc
+        self.sync_line(self.mobject[1])  # 查阅UpdateFromFunc
+        self.interpolate_path(self.curve_path, alpha)
+
+    def interpolate_path(self, path, alpha):
+        families = list(zip(*[
+            mob.family_members_with_points()
+            for mob in [path, path.copy()]
+        ]))
+        for i, mobs in enumerate(families):
+            sub_alpha = self.get_sub_alpha(alpha, i, len(families))
+            path.pointwise_become_partial(path.copy(), 0, sub_alpha)
+'''
+
+
+class MakeBezier2(AnimationGroup):
+    CONFIG = {
+        "lag_ratio": 0,
+        "rate_func": smooth,
+        "run_time": 6,
+    }
+
+    def __init__(self, mobject, **kwargs):
+        assert (isinstance(mobject, BezierGenerator))
+        dot_anim = mobject.dot_anim
+        sync_line = mobject.sync_line
+        curve_path = mobject.curve_path
+        super().__init__(
+            UpdateFromAlphaFunc(mobject[0], dot_anim),
+            UpdateFromFunc(mobject[1], sync_line),
+            ShowCreation(curve_path),
+            **kwargs
+        )
+
+
+# 该使用例更加简洁
 class TestBezier(Scene):
     def construct(self):
-        dot_lst = [
-            np.array([-3, -3, 0]),
-            np.array([-3, 3, 0]),
-            np.array([3, 3, 0]),
-            np.array([3, -3, 0]),
-        ]
+        dot_lst = np.array([
+            [-3, -3, 0],
+            [-3, 3, 0],
+            [3, 3, 0],
+            [3, -3, 0],
+        ])
         obj = BezierGenerator(dot_lst)
-        dot_anim = obj.dot_anim  # 点的动画函数(本质是function)
-        line_anim = obj.sync_line  # 直线的更新函数(本质是Function)
-        path = obj.curve_path  # 形成的贝塞尔曲线(本质是VMobject)
-        obj[1].add_updater(line_anim)  # 给直线加上更新函数
-        self.add(obj)  # 添加点和直线物件
-        self.play(UpdateFromAlphaFunc(obj[0], dot_anim),  # 使点的位置从初始移动到结束,直线也随之更新
-                  ShowCreation(path),  # 播放贝塞尔曲线的创建动画,与上面的同步
-                  run_time=4
-                  )
+        self.play(MakeBezier2(obj))
